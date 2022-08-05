5c53b8c850bb8fd9906bb7debd6097037c19fd37
user <user@DESKTOP-BV1OPJF>
2022-08-05 18:47:59 +0900

    test

---
+++ ThrowsGG/src/main/webapp/js/repeat-password.js
@@ -0,0 +1,18 @@
+  var repeatpassword = document.getElementById('registerRepeatPassword');
+	document.getElementById('alert-success').style.display = 'none';
+		document.getElementById('alert-danger').style.display = 'none';
+	repeatpassword.onkeyup = function(){
+		var p1 = document.getElementById('registerPassword').value;
+      	var p2 = document.getElementById('registerRepeatPassword').value;
+      	if(p1 !="" || p2 != ""){
+			 if( p1 != p2 ) {
+				document.getElementById('alert-success').style.display = 'none';
+				document.getElementById('alert-danger').style.display = '';
+				
+		      } else{
+			document.getElementById('alert-danger').style.display = 'none';
+		         document.getElementById('alert-success').style.display = '';
+		      }
+}
+
+};
