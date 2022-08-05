2aec0fd98e247f59daa9a9ade246ec3bc89d8b5f
user <user@DESKTOP-BV1OPJF>
2022-08-05 17:10:42 +0900

    회원 가입 완성

--- ThrowGG/src/main/webapp/js/repeat-password.js
+++ ThrowGG/src/main/webapp/js/repeat-password.js
@@ -1,19 +1,21 @@
-  var repeatpassword = document.getElementById('registerRepeatPassword');
-  document.getElementById('alert-danger').style.display = 'none';
+  	
+  	document.getElementById('alert-danger').style.display = 'none';
 	document.getElementById('alert-success').style.display = 'none';
 	
+
+	var repeatpassword = document.getElementById('registerRepeatPassword');
 	repeatpassword.onkeyup = function(){
-		var p1 = document.getElementById('registerPassword');
-      	var p2 = document.getElementById('registerRepeatPassword');
+		var p1 = document.getElementById('registerPassword').value;
+      	var p2 = document.getElementById('registerRepeatPassword').value;
+      	
       	if(p1 !="" || p2 != ""){
 			 if( p1 != p2 ) {
 				document.getElementById('alert-success').style.display = 'none';
 				document.getElementById('alert-danger').style.display = '';
 				
 		      } else{
-			document.getElementById('alert-danger').style.display = 'none';
+				document.getElementById('alert-danger').style.display = 'none';
 		         document.getElementById('alert-success').style.display = '';
 		      }
-}
-
+}	
 };
